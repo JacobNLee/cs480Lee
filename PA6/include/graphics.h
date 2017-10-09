ead47def6c55022ac8018c3caf9eb3bca99ec42b
JacobLee <JacobLee@nevada.unr.edu>
2017-10-08 23:35:26 -0700

    Attempting to get image magic to work

---
+++ PA6/include/graphics.h
@@ -0,0 +1,42 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <iostream>
+using namespace std;
+
+#include "graphics_headers.h"
+#include "mycamera.h"
+#include "shader.h"
+#include "object.h"
+
+class Graphics
+{
+  public:
+    Graphics();
+    ~Graphics();
+    bool Initialize(int width, int height, string shaders[]);
+    void Update(unsigned int dt);
+    void Render();
+
+    Object* getCube();
+
+    
+  private:
+    std::string ErrorString(GLenum error);
+
+    MyCamera *m_camera;
+    Shader *m_shader;
+
+    GLint m_projectionMatrix;
+    GLint m_viewMatrix;
+    GLint m_modelMatrix;
+    GLint m_modelMatrix2;
+
+    Object *m_cube;
+    
+    
+
+    
+};
+
+#endif /* GRAPHICS_H */
---
+++ PA6/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./PA6 --v ../shaders/passthrough.vert --f ../shaders/passthrough.frag --m ../models/cube.obj
